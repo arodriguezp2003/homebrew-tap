e554d3315815d42257229cdfbcf4fc6e4bcdae80
Alejandro Rodriguez <alejandro.rodriguez@ionix.cl>
2026-05-02 23:34:01 -0400

    fix: update SHA256 hashes for v1.0.0 rebuild

--- Formula/intercept.rb
+++ Formula/intercept.rb
@@ -7,14 +7,14 @@ class Intercept < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/arodriguezp2003/intercepthq/releases/download/v1.0.0/intercept-1.0.0-darwin-arm64.tar.gz"
-      sha256 "9dd9a61185045693022c824745e24278b1eb8d7d304b2d39ceb9d164f6885387"
+      sha256 "a190bdeba40fc548a8e50cefa25011206e99bfb9ebbffb92990f99064cc18b0b"
 
       def install
         bin.install "intercept-darwin-arm64" => "intercept"
       end
     else
       url "https://github.com/arodriguezp2003/intercepthq/releases/download/v1.0.0/intercept-1.0.0-darwin-amd64.tar.gz"
-      sha256 "9d948de86f56967a124e68f21a1880c2dd7c4887350270abf3186230080e0828"
+      sha256 "fed3d4e6e3363a54e5d51d18a98e248a8bcad95414e62548356ae92e7a7a2eab"
 
       def install
         bin.install "intercept-darwin-amd64" => "intercept"
@@ -25,14 +25,14 @@ class Intercept < Formula
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/arodriguezp2003/intercepthq/releases/download/v1.0.0/intercept-1.0.0-linux-arm64.tar.gz"
-      sha256 "3e5fd661748442d6dd36f734464337a9669b87f9d7bdc45701674d42cbb37caf"
+      sha256 "f7f76b80629b6017a9e42cd0394e318f127455f593081959e2b34244a2837c10"
 
       def install
         bin.install "intercept-linux-arm64" => "intercept"
       end
     else
       url "https://github.com/arodriguezp2003/intercepthq/releases/download/v1.0.0/intercept-1.0.0-linux-amd64.tar.gz"
-      sha256 "1b8009a013330ca69e64c16d09a6600ce8e841fc30954e5d66433f916c42c062"
+      sha256 "9e1d6ac6cafd893e9d83fecfbb0be69415e6b24193732460a2ef3ca9447c5279"
 
       def install
         bin.install "intercept-linux-amd64" => "intercept"
